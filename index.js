9a43fbf3614e81957411628d94a8ba0c99dd0468
SEIYA <skkasans@gmail.com>
2020-07-21 00:11:58 +0900

    todoリストの追加を実装

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 "use strict";
 
-const buttonList = document.querySelectorAll('button');
+const button = document.getElementById('add-button');
 
-[...buttonList].forEach((button) => {
-  button.addEventListener ('click', () => {
-    const li = button.closest('li');
+button.addEventListener('click', () => {
+  const input = document.getElementById('new-todo');
+
+  const li = document.createElement('li');
+  li.innerText = input.value;
+
+  const doneButton = document.createElement('button');
+  doneButton.innerText = 'done';
+  li.appendChild(doneButton);
+
+  doneButton.addEventListener('click', () => {
+    const li = doneButton.closest('li');
     li.classList.add('done');
   });
-});
 
+  const ul = document.querySelector('ul');
+  ul.appendChild(li);
+})
